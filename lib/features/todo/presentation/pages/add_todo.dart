f2ee560e2d6127a3ab582235f46cc65056e4b7bd
elin <elin.simonsson@outlook.com>
2023-11-21 15:50:12 +0100

    added title and text for selecting date

--- lib/features/todo/presentation/pages/add_todo.dart
+++ lib/features/todo/presentation/pages/add_todo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:organize_me/core/constants/colors.dart';
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -10,9 +11,71 @@ class AddTodoPage extends StatefulWidget {
 class _AddTodoPageState extends State<AddTodoPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Scaffold(
-        body: Text("Add todo"),
+    return Scaffold(
+        body: Column(
+      children: [
+        const SizedBox(
+          height: 60,
+        ),
+        const Text(
+          "Add new todo",
+          style: TextStyle(fontSize: 30),
+        ),
+        _buildSelectDate(context)
+        //Container(
+        // margin: const EdgeInsets.symmetric(horizontal: 20),
+        // width: double.infinity,
+        // child: Card(
+        //   color: lightGreen,
+        //   child: Container(
+        //     width: 200,
+        //     padding: const EdgeInsets.all(10),
+        //     child: const Column(
+        //       crossAxisAlignment: CrossAxisAlignment.start,
+        //       children: [
+        //         Row(
+        //           children: [
+        //             SizedBox(
+        //               width: 10,
+        //             ),
+        //             Text(
+        //               "Select date",
+        //             ),
+        //           ],
+        //         ),
+        //         TextField(
+        //           decoration: InputDecoration(
+        //               contentPadding: EdgeInsets.only(left: 10),
+        //               labelText: "Enter date"),
+        //         ),
+        //  ,
+        //),
+      ],
+    ));
+  }
+
+  Container _buildSelectDate(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+      width: double.infinity,
+      height: 80,
+      child: Card(
+        color: lightGreen,
+        child: InkWell(
+          onTap: () {
+            //TODO  Navigate to other page to select date
+          },
+          child: Container(
+              width: 300,
+              height: double.infinity,
+              padding: const EdgeInsets.all(10),
+              child: const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Select date",
+                    style: TextStyle(fontSize: 20, color: darkPurple),
+                  ))), // TODO if the date is selected, show it here
+        ),
       ),
     );
   }
