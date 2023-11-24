1879ce32c63878c4b5c2a56da792a9610ad0f020
elin <elin.simonsson@outlook.com>
2023-11-24 14:37:11 +0100

    added title and description textfields in add_todo.dart

--- lib/features/todo/presentation/pages/add_todo.dart
+++ lib/features/todo/presentation/pages/add_todo.dart
@@ -9,6 +9,16 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleEditingController = TextEditingController();
+  TextEditingController descriptionEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    titleEditingController.dispose();
+    descriptionEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,27 +64,73 @@ class _AddTodoPageState extends State<AddTodoPage> {
     ));
   }
 
-  Container _buildSelectDate(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-      width: double.infinity,
-      height: 80,
-      child: Card(
-        color: lightGreen,
-        child: InkWell(
-          onTap: () {
-            //TODO  Navigate to other page to select date
-          },
-          child: Container(
-              width: 300,
-              height: double.infinity,
-              padding: const EdgeInsets.all(10),
-              child: const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Select date",
-                    style: TextStyle(fontSize: 20, color: darkPurple),
-                  ))), // TODO if the date is selected, show it here
+  Expanded _buildSelectDate(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+        width: double.infinity,
+        child: Column(
+          children: [
+            Card(
+              color: lightGreen,
+              child: InkWell(
+                onTap: () {
+                  //TODO Navigate to other page to select date
+                },
+                child: Container(
+                  width: 300,
+                  height: 80, // Set a specific height
+                  padding: const EdgeInsets.all(10),
+                  child: const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Select date",
+                      style: TextStyle(fontSize: 20, color: darkPurple),
+                    ),
+                  ), // TODO if the date is selected, show it here
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: TextField(
+                controller: titleEditingController,
+                decoration: InputDecoration(
+                    hintText: "Enter title",
+                    filled: true,
+                    fillColor: lightGreen,
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        titleEditingController.text = "";
+                      },
+                      child: const Icon(Icons.close),
+                    )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: TextField(
+                controller: descriptionEditingController,
+                minLines: 5,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  hintText: "Enter description",
+                  filled: true,
+                  fillColor: lightGreen,
+                  suffixIcon: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 0, top: 0, right: 0, bottom: 100),
+                    child: GestureDetector(
+                      onTap: () {
+                        descriptionEditingController.text = "";
+                      },
+                      child: const Icon(Icons.close),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
